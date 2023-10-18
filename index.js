68eb0764797e42c284babe0ae574268d8b4a3f3d
rikords <iamhakanduyar@gmail.com>
2023-10-18 23:56:26 +0300

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    console.log(this.innerHTML);
+    this.style.color = "white";
   });
 }
 

53c56f32a74a5a85f7aecb50bbdeb5a743d9463f
rikords <iamhakanduyar@gmail.com>
2023-10-19 23:53:18 +0300

    update

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    this.style.color = "white";
+    var buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+      default:
+        console.log(buttonInnerHTML);
+    }
   });
 }
-
-// var audio = new Audio("sounds/tom-1.mp3");
-//     audio.play();
